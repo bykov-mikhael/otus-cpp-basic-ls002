98d7ad1ca86ca51a9f8c180083549ca734e23a6f
usr <usr@nb-ide>
2023-10-20 00:29:04 +0300

    changes readme.md file

---
+++ example.cpp
@@ -0,0 +1,11 @@
+#include "example.h"
+
+#include <iostream>
+
+void example_function() {
+	std::cout << "Hello from example function! Test 001" << std::endl;
+}
+
+void todo_move_me_out() {
+	std::cout << "This function should be located in the separated source file. Test 002" << std::endl;
+}
