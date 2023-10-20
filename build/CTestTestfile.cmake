84cfcf80f9d7f5c17a1ac4d0284f9a177a9da1e5
bykov-mikhael <bykov.mikhael@yandex.ru>
2023-10-21 00:13:21 +0300

    Удалена директория build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/usr/git/otus-cpp-basic-ls002
-# Build directory: /home/usr/git/otus-cpp-basic-ls002/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
