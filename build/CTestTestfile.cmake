013fd9aca61bf986221eb7d4abe615aa2a72e47f
bykov-mikhael <bykov.mikhael@yandex.ru>
2023-10-21 00:14:30 +0300

    build deleted

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/usr/git/otus-cpp-basic-ls002
+# Build directory: /home/usr/git/otus-cpp-basic-ls002/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
